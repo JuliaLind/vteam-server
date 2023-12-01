7b28b3794d9bc2504e69db46e80ff1aedd8b081a
idaloof <99144519+idaloof@users.noreply.github.com>
2023-12-01 19:28:22 +0100

    Restructured routes directory

--- server/routes/v1/index.js
+++ server/routes/v1/index.js
@@ -2,23 +2,23 @@ import express from "express";
 // import some model from some file
 
 import adminRouter from "./admin/index.js";
+import userRouter from "./user/index.js";
 import bikesRouter from "./bikes.js";
 import cardRouter from "./card.js";
 import citiesRouter from "./cities.js";
 import loginRouter from "./login.js";
 import registerRouter from "./register.js";
-import userRouter from "./user/index.js";
 import zonesRouter from "./zones.js";
 
 const router = express.Router();
 
 router.use("/admin", /** + authModel.checkToken ? */ adminRouter);
+router.use("/user", userRouter);
 router.use("/bikes", bikesRouter);
 router.use("/card", cardRouter);
 router.use("/cities", citiesRouter);
 router.use("login", loginRouter);
 router.use("/register", registerRouter);
-router.use("/user", userRouter);
 router.use("zones", zonesRouter);
 
 export default router;
