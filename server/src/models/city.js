edf32e33c6810d4acde496da69124c6cd2a260cf
Julia Lind <juli22@student.bth.se>
2023-12-04 00:58:50 +0100

    city model done for now, will add more tests next week

--- server/src/models/city.js
+++ server/src/models/city.js
@@ -13,6 +13,48 @@ const city = {
         return result[0].map((zone) => {
             return this.adjTypes(zone);
         });
+    },
+    allZones: async function() {
+        const result = await db.queryNoArgs(`CALL all_zones();`);
+
+        return result[0].map((zone) => {
+            return this.adjTypes(zone);
+        });
+    },
+    /**
+     * 
+     * @param {String} cityId 
+     * @returns {Object}
+     */
+    single: async function(cityId) {
+        const result = await db.queryWithArgs(`CALL single_city(?);`, [cityId]);
+
+        return this.adjTypes(result[0][0]);
+    },
+
+    /**
+     * 
+     * @param {String} cityId 
+     * @returns {Object}
+     */
+    zonesInCity: async function(cityId) {
+        const result = await db.queryWithArgs(`CALL zones_in_city(?);`, [cityId]);
+
+        return result[0].map((zone) => {
+            return this.adjTypes(zone);
+        });
+    },
+    /**
+     * 
+     * @param {Int} bikeId 
+     * @returns {Object}
+     */
+    bikeZones: async function(bikeId) {
+        const result = await db.queryWithArgs(`CALL bike_zones(?);`, [bikeId]);
+
+        return result[0].map((zone) => {
+            return this.adjTypes(zone);
+        });
     }
 }
 
