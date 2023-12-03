d262ce6f5c43975c2d0f61affc553750c46aa6cd
Julia Lind <juli22@student.bth.se>
2023-12-04 00:31:15 +0100

    user model done for now, will add tests for uncivered parts next week

--- server/src/models/user.js
+++ server/src/models/user.js
@@ -34,12 +34,70 @@ const user = {
         });
     },
     /**
+     * body should contain Github Token
+     * @param {*} req 
+     * @param {*} res 
+     * @param {*} next 
+     */
+    login: async function(req, res, next) {
+        const email = this.extractEmail(req.body.token)
+        const payload = await db.queryWithArgs(`CALL user_login(?);`, [email]);
+;
+        const jwtToken = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: "24h" });
+        return res.json({
+            data: {
+                type: "success",
+                message: "User logged in",
+                user: payload,
+                token: jwtToken
+            }
+        });
+    },
+    /**
      * 
      * @param {String | Int} what id or email to search for, for wilcard search add % before, after or both
      * @return {Array} if the search string is not a wildcard the array will only contain one object
      */
     userSearch: async function(what) {
         const result = await db.queryWithArgs(`CALL user_search(?);`, [what]);
+        return result[0].map((user) => {
+            return this.adjTypes(user);
+        });
+    },
+    /**
+     * 
+     * @param {Int} userId 
+     * @param {Boolean} active 
+     * @returns {Object}
+     */
+    updStatus: async function(userId, active) {
+        const result = await db.queryWithArgs(`CALL upd_user_status(?, ?);`, [userId, active]);
+        return this.adjTypes(result[0][0]);
+    },
+    /**
+     * 
+     * @param {Int} userId
+     * @param {String} email
+     * @returns {Object}
+     */
+    updEmail: async function(userId, email) {
+        const result = await db.queryWithArgs(`CALL upd_user_email(?, ?);`, [userId, email]);
+        return this.adjTypes(result[0][0]);
+    },
+    /**
+     * 
+     * @param {Int} offset
+     * @param {Int} limit
+     * @returns {Array}
+     */
+    allPag: async function(offset, limit) {
+        const result = await db.queryWithArgs(`CALL upd_user_email(?, ?);`, [offset, limit]);
+        return result[0].map((user) => {
+            return this.adjTypes(user);
+        });
+    },
+    all: async function() {
+        const result = await db.queryNoArgs(`CALL all_users_pag();`, [offset, limit]);
         return result[0].map((user) => {
             return this.adjTypes(user);
         });
