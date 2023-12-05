ee16fbad0c0111b6c74694e8d86bf530fa568850
idaloof <99144519+idaloof@users.noreply.github.com>
2023-12-05 22:16:04 +0100

    Prepared for adding trips all route

--- server/routes/v1/admin/index.js
+++ server/routes/v1/admin/index.js
@@ -3,8 +3,8 @@ import express from "express";
 import bikesRouter from "./bikes.js";
 import feedRouter from "./feed.js";
 import simulationRouter from "./simulate.js";
-import transactionsRouter from "../transactions.js";
-import tripsRouter from "../trips.js";
+import transactionsRouter from "./transactions.js";
+import tripsRouter from "./trips.js";
 import usersRouter from "./users.js";
 
 const router = express.Router();
