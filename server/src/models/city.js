1c73a10b26d02345d6812e6df2cd15f933480253
Julia Lind <juli22@student.bth.se>
2023-12-07 17:56:30 +0100

    all moethods in city model manually tested and are returning expected results

--- server/src/models/city.js
+++ server/src/models/city.js
@@ -1,14 +1,39 @@
 import { db } from "./db.js"
 
 const city = {
+    /**
+     * "prettifies" a zone or city
+     * object. If the value of speed_limit
+     * attribute is 0 the attribute is removed.
+     * Value of the geometry object
+     * is converted from string to geoJson
+     * geometry object
+     * @param {Object} zoneObj 
+     * @returns {Object} zoneObj 
+     */
     adjTypes: function(zoneObj) {
+        zoneObj.speed_limit === null ? delete zoneObj.speed_limit : zoneObj.speed_limit
         return {
             ...zoneObj,
             geometry: JSON.parse(zoneObj.geometry)
         };
     },
     /**
-     * 
+     * Returns data for a single city:
+     * id, name, general speed limit, and
+     * geometry.
+     * @param {String} cityId 
+     * @returns {Promise<Object>} single city
+     */
+    single: async function(cityId) {
+        const result = await db.queryWithArgs(`CALL single_city(?);`, [cityId]);
+
+        return this.adjTypes(result[0][0]);
+    },
+    /**
+     * Returns an array with all cities.
+     * A cityobject contains id, name, general speed limit, and
+     * geometry
      * @returns {Promise<Array>} all cities
      */
     all: async function() {
@@ -18,8 +43,26 @@ const city = {
             return this.adjTypes(zone);
         });
     },
+        /**
+     * Returns array with all zone objects
+     * in a city. Zoneobject contains:
+     * id, zone_id (type of zone),
+     * descr, city_id, geometry and speed_limit. If a zone does
+     * no have a speed_limit it will not have the attribute
+     * @param {String} cityId 
+     * @returns {Promise<Array>} All
+     * zones in a city 
+     */
+    zonesInCity: async function(cityId) {
+        const result = await db.queryWithArgs(`CALL zones_in_city(?);`, [cityId]);
+
+        return result[0].map((zone) => {
+            return this.adjTypes(zone);
+        });
+    },
+
     /**
-     * 
+     * Returns all zones in all cities
      * @returns {Promise<Array>} all zones
      */
     allZones: async function() {
@@ -29,35 +72,20 @@ const city = {
             return this.adjTypes(zone);
         });
     },
-    /**
-     * 
-     * @param {String} cityId 
-     * @returns {Promise<Object>} single city
-     */
-    single: async function(cityId) {
-        const result = await db.queryWithArgs(`CALL single_city(?);`, [cityId]);
 
-        return this.adjTypes(result[0][0]);
-    },
+
 
     /**
-     * 
-     * @param {String} cityId 
-     * @returns {Promise<Array>} Zones in a city
-     */
-    zonesInCity: async function(cityId) {
-        const result = await db.queryWithArgs(`CALL zones_in_city(?);`, [cityId]);
-
-        return result[0].map((zone) => {
-            return this.adjTypes(zone);
-        });
-    },
-    /**
-     * 
+     *  Returns city id, geometry and
+     * general speed limit for a city,
+     * and an array with
+     * forbidden zone objects
+     * in the same city as the bike is
+     * registered to: geometry, zone_id
+     * (3) ans speed_limit (0)
      * @param {Number} bikeId 
-     * @returns {Promise<Object>} forbidden zones
-     * in the same city as the bike is registered to
-     * and the city limits
+     * @returns {Promise<Object>} 
+     * 
      */
     bikeZones: async function(bikeId) {
         const result = await db.queryWithArgs(`CALL bike_zones(?);`, [bikeId]);
