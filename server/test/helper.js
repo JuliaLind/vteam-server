8b2baec77abddd275eada46a563500372a78e0c0
Julia Lind <juli22@student.bth.se>
2023-12-08 00:42:47 +0100

    tested out a new way to insert rows individually, to avoid mixed item order in tests due to db optimization. Appears to be working well

---
+++ server/test/helper.js
@@ -0,0 +1,39 @@
+import { db } from "../src/models/db.js";
+import { trips } from './dummy-data/trips.js'
+
+export const insertTrips = async function () {
+    const conn = await db.pool.getConnection();
+    try {
+        await conn.beginTransaction();
+
+        for (const elem of trips) {
+            await conn.query('INSERT INTO trip VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?);', [
+                elem.id,
+                elem.user_id,
+                elem.bike_id,
+                elem.start_time,
+                elem.end_time,
+                JSON.stringify(elem.start_pos),
+                JSON.stringify(elem.end_pos),
+                elem.start_cost,
+                elem.var_cost,
+                elem.park_cost
+            ]);
+            const id = await conn.query('SELECT MAX(id) AS last_id FROM trip');
+            elem.id = id;
+        }
+
+        await conn.commit();
+
+    } catch (err) {
+        if (conn) {
+            await conn.rollback();
+        }
+        throw err;
+    } finally {
+        if (conn) {
+            await conn.end();
+        }
+    }
+    return trips;
+}
