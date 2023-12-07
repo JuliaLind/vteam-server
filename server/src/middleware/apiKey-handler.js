26a02df61f099d1af23832c6abb402fadf92658f
idaloof <99144519+idaloof@users.noreply.github.com>
2023-12-07 14:09:04 +0100

    Created a middleware function to check API key

---
+++ server/src/middleware/apiKey-handler.js
@@ -0,0 +1,35 @@
+import express from "express";
+import apiKeyModel from "../models/api-key.js";
+
+/**
+ *
+ * @param {express.Request} req
+ * @param {express.Response} res
+ * @param {express.NextFunction} next
+ */
+async function apiKeyHandler(req, res, next) {
+
+    const apiKey = req.headers['x-api-key'];
+
+    const apiKeyString = Array.isArray(apiKey) ? apiKey[0] : apiKey;
+
+    if (!apiKeyString) {
+        return res.status(401).json({
+            success: false,
+            message: 'API key is required.'
+        });
+    }
+
+    const isValidKey = await apiKeyModel.checkOne(apiKeyString);
+
+    if (!isValidKey) {
+        return res.status(401).json({
+            success: false,
+            message: 'Invalid or missing API key. Access denied.'
+        });
+    }
+
+    next();
+}
+
+export default apiKeyHandler;
