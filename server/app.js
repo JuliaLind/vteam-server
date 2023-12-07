698b0ac37e5e83c8849257209df6cba174021b1c
idaloof <99144519+idaloof@users.noreply.github.com>
2023-12-07 14:06:45 +0100

    Divided app.js into two files to be able to run tests

--- server/app.js
+++ server/app.js
@@ -7,12 +7,12 @@ import morgan from "morgan";
 dotenv.config();
 // dotenv.config({ path: '.env' });
 
-// import errorHandler from "./middleware/errors.js";
 import apiRouter from "./routes/v1/index.js";
+import errorHandler from "./src/middleware/error-handler.js";
+// import apiKeyHandler from "./src/middleware/apiKey-handler.js";
 
 
 const app = express();
-const port = 1337;
 
 app.use(morgan("dev"));
 
@@ -22,10 +22,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 // TODO: Här kan vi lägga in en middleware för att kolla API-nyckel?
+// app.use(apiKeyHandler);
 
 app.use("/v1", apiRouter);
 
-// app.use(errorHandler);
+app.use(errorHandler);
 
 
 /**
@@ -37,6 +38,5 @@ app.get("/", async (req, res) => {
         data: "Hej från team2 server",
     });
 });
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-});
+
+export default app;
