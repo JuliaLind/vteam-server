9b5dbfe44f2730e0beca5e6262f0c3b735a331eb
idaloof <99144519+idaloof@users.noreply.github.com>
2023-12-27 22:25:23 +0100

    Made sql setup code available again

--- mariadb/setup-bikes-local.sql
+++ mariadb/setup-bikes-local.sql
@@ -1,22 +1,22 @@
--- DROP DATABASE IF EXISTS `bikes`;
--- CREATE DATABASE `bikes`;
--- USE `bikes`;
+DROP DATABASE IF EXISTS `bikes`;
+CREATE DATABASE `bikes`;
+USE `bikes`;
 
--- source ./sql/ddl.sql;
--- source ./sql/insert-local.sql;
+source ./sql/ddl.sql;
+source ./sql/insert-local.sql;
 
 
--- source ./sql/view.sql;
--- source ./sql/functions.sql;
--- source ./sql/sp_payment.sql;
--- source ./sql/sp_user.sql;
--- source ./sql/sp_emp.sql;
--- source ./sql/sp_trip.sql;
--- source ./sql/sp_bike.sql;
--- source ./sql/sp_city.sql;
--- source ./sql/sp_card.sql;
--- source ./sql/sp_key.sql;
--- source ./sql/triggers.sql;
+source ./sql/view.sql;
+source ./sql/functions.sql;
+source ./sql/sp_payment.sql;
+source ./sql/sp_user.sql;
+source ./sql/sp_emp.sql;
+source ./sql/sp_trip.sql;
+source ./sql/sp_bike.sql;
+source ./sql/sp_city.sql;
+source ./sql/sp_card.sql;
+source ./sql/sp_key.sql;
+source ./sql/triggers.sql;
 
 
 
