06f5548ec161489b39d17ffcf4140a69db45b9c7
idaloof <99144519+idaloof@users.noreply.github.com>
2023-12-27 23:24:37 +0100

    Commented out the register route

--- server/routes/v1/register.js
+++ server/routes/v1/register.js
@@ -1,19 +1,19 @@
-import express from "express";
-// import some model from some file
+// import express from "express";
+// // import some model from some file
 
-const router = express.Router();
+// const router = express.Router();
 
-/**
- * @description User register route
- *
- * @param {express.Request} req Request object
- * @param {express.Response} res Response object
- * @param {express.NextFunction} next Next function
- *
- * @returns {void}
- */
-router.post("/", async (req, res, next) => {
-    // code here for registering a user
-});
+// /**
+//  * @description User register route
+//  *
+//  * @param {express.Request} req Request object
+//  * @param {express.Response} res Response object
+//  * @param {express.NextFunction} next Next function
+//  *
+//  * @returns {void}
+//  */
+// router.post("/", async (req, res, next) => {
+//     // code here for registering a user
+// });
 
-export default router;
+// export default router;
