b00410720c98688dbe7f90a203464da79a793113
idaloof <99144519+idaloof@users.noreply.github.com>
2024-01-08 10:12:37 +0100

    Cleaned up a bit and added public folder as express static folder

--- server/app.js
+++ server/app.js
@@ -2,7 +2,9 @@ import dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
-
+import path from "path";
+import apiRouter from "./routes/v1/index.js";
+import errorHandler from "./src/middleware/error-handler.js";
 
 const corsOptions = {
     origin: true,
@@ -12,42 +14,21 @@ const corsOptions = {
 dotenv.config();
 // dotenv.config({ path: '.env' });
 
-import apiRouter from "./routes/v1/index.js";
-import errorHandler from "./src/middleware/error-handler.js";
-// import apiKeyHandler from "./src/middleware/apiKey-handler.js";
-
 const app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(process.cwd(), 'public')));
 
 app.use(morgan("dev"));
 
 app.use(cors(corsOptions));
+
 app.use(express.json());
 
-app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
-
-// TODO: Avkommentera den här och rad 17 när det är dags.
-// app.use(apiKeyHandler);
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/v1", apiRouter);
 
 app.use(errorHandler);
-
-// just for testing via browser
-// import userModel from "./src/models/user.js";
-
-
-
-// /**
-//  * Just to check that database connection
-//  * is working.. will remove later
-//  */
-// app.get("/", async (req, res) => {
-//     const result = await userModel.allPag(10, 3);
-//     res.json(result);
-// });
-
-// app.listen(port, () => {
-//     console.log(`Server running on port ${port}`);
-// });
 
 export default app;
