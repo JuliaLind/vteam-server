c0ece8f6c095a4003edeb8fdaba34f4129a58b25
Julia Lind <juli22@student.bth.se>
2024-01-08 18:41:36 +0100

    chagede console.log to console.info to fic issue on Scrutinizer

--- server/server.js
+++ server/server.js
@@ -3,5 +3,5 @@ import app from "./app.js";
 const port = 1337;
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+    console.info(`Server running on port ${port}`);
 });
