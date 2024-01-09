f4a3f35d6332c1ca63549edec9a59964dc34d0c3
Julia Lind <juli22@student.bth.se>
2024-01-09 19:05:41 +0100

    removed commented out code

--- mariadb/setup-bikes.sql
+++ mariadb/setup-bikes.sql
@@ -2,22 +2,6 @@ DROP DATABASE IF EXISTS `bikes`;
 CREATE DATABASE `bikes`;
 USE `bikes`;
 
-
--- source ./mariadb/sql/ddl.sql;
--- source ./mariadb/sql/insert-test-and-bikes.sql;
--- source ./mariadb/sql/insert-bikes.sql;
--- source ./mariadb/sql/view.sql;
--- source ./mariadb/sql/functions.sql;
--- source ./mariadb/sql/sp_payment.sql;
--- source ./mariadb/sql/sp_user.sql;
--- source ./mariadb/sql/sp_emp.sql;
--- source ./mariadb/sql/sp_trip.sql;
--- source ./mariadb/sql/sp_bike.sql;
--- source ./mariadb/sql/sp_city.sql;
--- source ./mariadb/sql/sp_card.sql;
--- source ./mariadb/sql/sp_key.sql;
--- source ./mariadb/sql/triggers.sql;
-
 source ./sql/ddl.sql;
 source ./sql/insert-test-and-bikes.sql;
 source ./sql/insert-bikes.sql;
