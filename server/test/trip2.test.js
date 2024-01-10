4924bebd6587cb40ea32826c85dd2aa3ea8db632
Julia Lind <juli22@student.bth.se>
2024-01-11 00:42:47 +0100

    some more cleanup

--- server/test/trip2.test.js
+++ server/test/trip2.test.js
@@ -55,53 +55,59 @@ describe('trip model part 2', () => {
     });
 
     it('get all trips for a user', async () => {
-
         // user 6
-        let userTrips = dataAdj.filter((elem) => elem.user_id === users[2].id);
-        let res = await tripModel.userTrips(users[2].id);
+        const userTrips = dataAdj.filter((elem) => elem.user_id === users[2].id);
+        const res = await tripModel.userTrips(users[2].id);
         expect(res.length).to.equal(7);
         expect(res).to.deep.equal(userTrips);
-
-        // user 7
-        userTrips = dataAdj.filter((elem) => elem.user_id === users[3].id);
-        res = await tripModel.userTrips(users[3].id);
-        expect(res.length).to.equal(3);
-        expect(res).to.deep.equal(userTrips);
-
-        
+    });
+    it('get all trips for another user', async () => {
         // user 5
-        userTrips = dataAdj.filter((elem) => elem.user_id === users[1].id);
-        res = await tripModel.userTrips(users[1].id);
+        const userTrips = dataAdj.filter((elem) => elem.user_id === users[1].id);
+        const res = await tripModel.userTrips(users[1].id);
+        expect(res.length).to.equal(5);
+        expect(res).to.deep.equal(userTrips);
+    });
+    it('get all trips for one more user', async () => {
+        // user 5
+        const userTrips = dataAdj.filter((elem) => elem.user_id === users[1].id);
+        const res = await tripModel.userTrips(users[1].id);
         expect(res.length).to.equal(5);
         expect(res).to.deep.equal(userTrips);
     });
 
     it('get all trips for a user paginated', async () => {
-
         // user 6
-        let userTrips = dataAdj.filter((elem) => elem.user_id === users[2].id);
-        let res = await tripModel.userTripsPag(users[2].id, 2, 3);
+        const userTrips = dataAdj.filter((elem) => elem.user_id === users[2].id);
+        const res = await tripModel.userTripsPag(users[2].id, 2, 3);
         expect(res.length).to.equal(3);
         expect(res).to.deep.equal(userTrips.slice(2, 2 + 3));
-
-        res = await tripModel.userTripsPag(users[2].id, 3, 3);
+    });
+    it('get all trips for a user paginated another range', async () => {
+        // user 6
+        const userTrips = dataAdj.filter((elem) => elem.user_id === users[2].id);
+        const res = await tripModel.userTripsPag(users[2].id, 3, 3);
         expect(res.length).to.equal(3);
         expect(res).to.deep.equal(userTrips.slice(3, 3 + 3));
-
-        res = await tripModel.userTripsPag(users[2].id, 3, 5);
-        // because only 7 elemens in tital so, 3 + 4
+    });
+    it('get all trips for a user paginated out of range ok', async () => {
+        // user 6
+        const userTrips = dataAdj.filter((elem) => elem.user_id === users[2].id);
+        const res = await tripModel.userTripsPag(users[2].id, 3, 5);
+        // because only 7 elemens in total so, 3 + 4
         expect(res.length).to.equal(4);
         expect(res).to.deep.equal(userTrips.slice(3, 3 + 5));
-
-
+    });
+    it('get all trips for another user paginated', async () => {
         // user 5
-        userTrips = dataAdj.filter((elem) => elem.user_id === users[1].id);
-        res = await tripModel.userTripsPag(users[1].id, 2, 3);
+        const userTrips = dataAdj.filter((elem) => elem.user_id === users[1].id);
+        const res = await tripModel.userTripsPag(users[1].id, 2, 3);
         expect(res.length).to.equal(3);
         expect(res).to.deep.equal(userTrips.slice(2, 2 + 3));
-
+    });
+    it('get all trips for non-existing user, not ok', async () => {
         // non-existsing user
-        res = await tripModel.userTripsPag(0, 2, 3);
+        const res = await tripModel.userTripsPag(0, 2, 3);
         expect(res.length).to.equal(0);
         expect(res).to.deep.equal([]);
     });
